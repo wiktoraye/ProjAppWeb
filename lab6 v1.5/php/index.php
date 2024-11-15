214322ccfb9163a6e4eb44152c9f66dbccc73bdc
wiktoraye <115210668+wiktoraye@users.noreply.github.com>
2024-11-16 00:11:47 +0100

    Update index.php

--- lab6 v1.5/php/index.php
+++ lab6 v1.5/php/index.php
@@ -39,7 +39,7 @@ $strona_content = PokazPodstrone($page_id);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Language" content="pl" />
-    <meta name="Author" content="Damian Todorowski" />
+    <meta name="Author" content="Wiktor Jakacki" />
     <title>Moje Hobby: Projektowanie Ubrań</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
